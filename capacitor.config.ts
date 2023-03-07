ffb47095b72bd28547bd93c4d48d22779eee4787
bachiito <riveraabraham332@gmail.com>
2023-03-06 21:32:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Toolbox',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
